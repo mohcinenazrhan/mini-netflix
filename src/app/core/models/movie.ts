6b382f682fd9f0e401431a480e61cd0a8e41392b
mohcinenazrhan <nazrhan.mohcine@gmail.com>
2019-11-03 22:02:47 +0100

    feat: add model of movie object

---
+++ src/app/core/models/movie.ts
@@ -0,0 +1,28 @@
+export default class Movies {
+	id: number;
+	Title: string;
+	Year: string;
+	Rated: string;
+	Released: string;
+	Runtime: string;
+	Genre: string;
+	Director: string;
+	Writer: string;
+	Actors: string;
+	Plot: string;
+	Language: string;
+	Country: string;
+	Awards: string;
+	Poster: string;
+	Ratings: Array<object>;
+	Metascore: string;
+	imdbRating: string;
+	imdbVotes: string;
+	imdbID: string;
+	Type: string;
+	DVD: string;
+	BoxOffice: string;
+	Production: string;
+	Website: string;
+	Response: string;
+}
