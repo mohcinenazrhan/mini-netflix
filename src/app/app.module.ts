97e78954b962a8b7e2f181ec4bb763b84691f434
mohcinenazrhan <nazrhan.mohcine@gmail.com>
2019-11-04 23:18:58 +0100

    chore: config app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { DataService } from './data.service';
+import { MovieService } from './core/services';
+import { HttpClientModule } from '@angular/common/http';
+import { RoutingModule } from './routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent, SingleComponent, FavoritesComponent } from './pages';
+import { FooterComponent, HeaderComponent } from './shared/components';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		HomeComponent,
+		SingleComponent,
+		FavoritesComponent,
+		FooterComponent,
+		HeaderComponent
+	],
+	imports: [
+		BrowserModule,
+		RoutingModule,
+		InMemoryWebApiModule.forRoot(DataService),
+		HttpClientModule,
+		MDBBootstrapModule.forRoot()
+	],
+	providers: [ MovieService ],
+	bootstrap: [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule {}
