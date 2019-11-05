549b73ba2d484ef8c34b16e8761bf792dde758bb
mohcinenazrhan <nazrhan.mohcine@gmail.com>
2019-11-05 22:14:28 +0100

    feat: add single page

---
+++ src/app/pages/single/single.page.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { MovieService } from '../../core/services/movie.service';
+import { UserService } from '../../core/services/user.service';
+
+import { Location } from '@angular/common';
+import Movie from '../../core/models/movie';
+
+@Component({
+	templateUrl: './single.page.html',
+	styleUrls: [ './single.page.scss' ]
+})
+export class SingleComponent implements OnInit {
+	favorites = {};
+	movie: Movie;
+	poster: 'https://m.media-amazon.com/images/M/MV5BMDg2YzI0ODctYjliMy00NTU0LTkxODYtYTNkNjQwMzVmOTcxXkEyXkFqcGdeQXVyNjg2NjQwMDQ@.jpg';
+
+	constructor(
+		private route: ActivatedRoute,
+		private movieService: MovieService,
+		private location: Location,
+		private userService: UserService
+	) {}
+
+	ngOnInit(): void {
+		this.getMovie();
+		this.getFavorites();
+	}
+
+	getMovie(): void {
+		const id = +this.route.snapshot.paramMap.get('id');
+		this.movieService.getMovie(id).subscribe((movie: Movie) => {
+			console.log(movie);
+			this.movie = movie;
+		});
+	}
+
+	getFavorites(): void {
+		this.favorites = this.userService.getFavorites();
+	}
+
+	favoriteToggle(id: number): void {
+		this.userService.toggleUserFavorite(id);
+	}
+}
