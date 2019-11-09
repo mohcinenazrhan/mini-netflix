a0fd757e3ad52d78eee64456c66189645b50eafd
mohcinenazrhan <nazrhan.mohcine@gmail.com>
2019-11-09 16:10:56 +0100

    feat: add class active link menu

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,7 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
 	selector: 'app-header',
 	templateUrl: './header.component.html'
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+	constructor(public router: Router) {}
+}
